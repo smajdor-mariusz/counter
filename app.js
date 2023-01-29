cfbf5e38b74922634236381e19a1f0f1fdef8cad
Mariusz <smajdortech@gmail.com>
2023-01-29 17:58:23 +0100

    Counter decrease functionality

--- app.js
+++ app.js
@@ -1,9 +1,15 @@
 const counterEl = document.querySelector('.counter');
+const decreaseEl = document.querySelector('.decrease');
 const increaseEl = document.querySelector('.increase');
 
 let counter = 0;
 
 counterEl.innerText = counter;
+
+decreaseEl.addEventListener('click', () => {
+  counter--;
+  counterEl.innerText = counter;
+});
 
 increaseEl.addEventListener('click', () => {
   counter++;
