1c7775221e9b4b54e97c27d48d0d8a376ebe2e9a
Mariusz <smajdortech@gmail.com>
2023-01-29 17:59:43 +0100

    Counter reset functionality

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const counterEl = document.querySelector('.counter');
 const decreaseEl = document.querySelector('.decrease');
 const increaseEl = document.querySelector('.increase');
+const reset = document.querySelector('.reset');
 
 let counter = 0;
 
@@ -13,5 +14,10 @@ decreaseEl.addEventListener('click', () => {
 
 increaseEl.addEventListener('click', () => {
   counter++;
+  counterEl.innerText = counter;
+});
+
+reset.addEventListener('click', () => {
+  counter = 0;
   counterEl.innerText = counter;
 });
