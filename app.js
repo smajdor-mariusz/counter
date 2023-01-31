366ce0bd79664551c7f5581e2e9685845415de34
Mariusz <smajdortech@gmail.com>
2023-01-31 23:21:43 +0100

    Increase and descrase step functionality

--- app.js
+++ app.js
@@ -2,22 +2,32 @@ const counterEl = document.querySelector('.counter');
 const decreaseEl = document.querySelector('.decrease');
 const increaseEl = document.querySelector('.increase');
 const reset = document.querySelector('.reset');
+const stepEl = document.getElementById('step');
 
 let counter = 0;
 
 counterEl.innerText = counter;
 
-decreaseEl.addEventListener('click', () => {
-  counter--;
+function count(operator) {
+  if (operator === 'decrease') {
+    counter -= +stepEl.value;
+  } else if (operator === 'increase') {
+    counter += +stepEl.value;
+  } else {
+    counter = 0;
+  }
+
   counterEl.innerText = counter;
+}
+
+decreaseEl.addEventListener('click', () => {
+  count('decrease');
 });
 
 increaseEl.addEventListener('click', () => {
-  counter++;
-  counterEl.innerText = counter;
+  count('increase');
 });
 
 reset.addEventListener('click', () => {
-  counter = 0;
-  counterEl.innerText = counter;
+  count(null);
 });
