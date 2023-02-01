970e2be3f60f07dd2d764b3efaac716e18d5797a
Mariusz <smajdortech@gmail.com>
2023-02-01 12:29:57 +0100

    Save counter step and value in local storage

--- app.js
+++ app.js
@@ -4,9 +4,13 @@ const increaseEl = document.querySelector('.increase');
 const resetEl = document.querySelector('.reset');
 const stepEl = document.getElementById('step');
 
-let counter = 0;
+const LS_COUNTER_KEY = 'counter.counter';
+const LS_STEP_KEY = 'counter.step';
+
+let counter = +localStorage.getItem(LS_COUNTER_KEY) || 0;
 
 counterEl.innerText = counter;
+stepEl.value = localStorage.getItem(LS_STEP_KEY) || 1;
 
 function count(operator) {
   if (operator === 'decrease') {
@@ -17,8 +21,13 @@ function count(operator) {
     counter = 0;
   }
 
+  localStorage.setItem(LS_COUNTER_KEY, counter);
   counterEl.innerText = counter;
 }
+
+stepEl.addEventListener('input', () => {
+  localStorage.setItem(LS_STEP_KEY, stepEl.value);
+});
 
 decreaseEl.addEventListener('click', () => {
   count('decrease');
